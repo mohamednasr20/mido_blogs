d0da837b4b864ae0b3205e39358fe4691a364a5f
mohamednasr20 <mohamednasr8687@gmail.com>
2021-01-26 00:16:12 -0500

    add html pages and express

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-const http = require("http");
+const express = require("express");
 
-const server = http.createServer((req, res) => {
-  console.log("request made");
+const app = express();
+
+app.listen(3000);
+
+app.get("/", (req, res) => {
+  // res.send("<p>home page</p>");
+  res.sendFile("./views/home.html", { root: __dirname });
 });
 
-server.listen(3000, () => {
-  console.log("run server");
+app.get("/about", (req, res) => {
+  res.sendFile("./views/about.html", { root: __dirname });
+});
+
+app.get("/about-me", (req, res) => {
+  res.redirect("/about");
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile("./views/404.html", { root: __dirname });
 });
