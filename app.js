86527b1fbeb952660d4a15268484e5881c1c01a8
mohamednasr20 <mohamednasr8687@gmail.com>
2021-01-26 23:32:17 -0500

    add view engine

--- app.js
+++ app.js
@@ -2,21 +2,28 @@ const express = require("express");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.listen(3000);
 
 app.get("/", (req, res) => {
   // res.send("<p>home page</p>");
-  res.sendFile("./views/home.html", { root: __dirname });
+  // res.sendFile("./views/home.html", { root: __dirname });
+  res.render("index");
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about");
 });
 
-app.get("/about-me", (req, res) => {
-  res.redirect("/about");
+app.get("/blogs/create", (req, res) => {
+  res.render("create");
 });
+
+// app.get("/about-me", (req, res) => {
+//   res.redirect("/about");
+// });
 
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404");
 });
