96c3e0dd141196d6bb462741abfdb16c4f08886e
mohamednasr20 <mohamednasr8687@gmail.com>
2021-01-31 00:29:05 -0500

    add all blogs from db

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const Blog = require("./models/blog");
+const dbURI = require("./dbURI");
 
 const app = express();
-
-const dbURI =
-  "mongodb+srv://mohamednasr86:<passward></passward>@cluster0.cfnr6.mongodb.net/node-tuts?retryWrites=true&w=majority";
 
 mongoose
   .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -16,76 +14,25 @@ app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 
-app.get("/add-blog", (req, res) => {
-  const blog = new Blog({
-    title: "new blog",
-    snippet: "about my new blog",
-    body: "more about my new blog",
-  });
-
-  blog
-    .save()
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.get("/all-blogs", (req, res) => {
-  Blog.find()
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.get("/single-blog", (req, res) => {
-  Blog.findById("6014f1b9d3b79f9aa79ccc3e")
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
 app.get("/", (req, res) => {
-  const blogs = [
-    {
-      title: "yoshi finds eggs",
-      snippet:
-        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam, ipsum",
-    },
-    {
-      title: "mario finds stars",
-      snippet:
-        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam, ipsum",
-    },
-    {
-      title: "how to defeat bowser",
-      snippet:
-        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam, ipsum",
-    },
-  ];
-  res.render("index", { title: "Home", blogs });
+  res.redirect("/blogs");
 });
 
 app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
 });
 
+app.get("/blogs", (req, res) => {
+  Blog.find()
+    .then((reuslt) => {
+      res.render("index", { title: "all blogs", blogs: reuslt });
+    })
+    .catch((err) => console.log(err));
+});
+
 app.get("/blogs/create", (req, res) => {
   res.render("create", { title: "Create A New Blog" });
 });
-
-// app.get("/about-me", (req, res) => {
-//   res.redirect("/about");
-// });
-
 app.use((req, res) => {
   res.status(404).render("404", { title: "404" });
 });
---
+++ dbURI.js
@@ -0,0 +1,4 @@
+const dbURI =
+  "mongodb+srv://mohamednasr86:m4102005m@cluster0.cfnr6.mongodb.net/node-tuts?retryWrites=true&w=majority";
+
+module.exports = dbURI;
